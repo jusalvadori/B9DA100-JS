35282d5c85c00cf1b6d5c1d5163a6d666a4a61c9
Juliana <juliana.salvadori@gmail.com>
2019-12-16 21:46:45 +0000

    added insert bill
    added insert bill

--- billManagement/bill_management.py
+++ billManagement/bill_management.py
@@ -1,43 +1,60 @@
-#import csv
+import datetime
 
-def read_bills():
-    
-    return [ [col.strip() for col in line.strip().split(',') ] for line in open('bills.csv') if len(line) > 1 ] 
-    
-#    bills = [] 
-#    bills_file = open('bills.csv') 
-#    for line in bills_file: 
-#        bills.append(line.strip().split(',')) 
-#        bills[-1][-1] = bills[-1][-1].strip()  
-#    return bills  
+class bill_management(object):
+    def __init__(self):
+        self.bills = []
 
-def display_menu():
-    print('Welcome to bill management company') 
-    print('1: View bills\n2: Insert a bill\n3: Reports\n4:T&C\n5: Exit') 
+    def read_bills(self):
+        self.bills = []
+        bills_file = open('bills.csv') 
+        for line in bills_file: 
+            self.bills.append(line.strip().split(',')) 
+            self.bills[-1][-1] = self.bills[-1][-1].strip()  
     
-def view_bills(bills):
-    for bill in bills:
-        print(bill[0], bill[1], bill[3], bill[4], bill[5], bill[6])
+    def write_bills(self):
+        with open('bills.csv', mode='w') as bill_file:
+            for bill in self.bills:            
+                bill_file.write(', '.join(bill) + '\n')
     
-def process_choice(bills):
-    choice = input('Please enter an option:') 
-    while choice != '5':    # check the version of python to identify the type of return from input function 
-        if choice == '1':
-            view_bills(bills)
+    def display_menu(self):
+        print('Welcome to bill management company') 
+        print('1: View bills\n2: Insert a bill\n3: Reports\n4: T&C\n5: Exit') 
         
-        choice = input('Please enter an option:') 
-
-def write_bills(bills):
-    with open('bills.csv', mode='w') as bill_file:
-        for bill in bills:            
-            #bill_file.write(', '.join(bill))  # this returns an error (add to the test file) as it will write everything in the same line
-            bill_file.write(', '.join(bill) + '\n')
+    def view_bills(self):
+        for bill in self.bills:
+            print(bill[0], bill[1], bill[2], bill[3], bill[4], bill[5], bill[6])
             
-def main():
-    bills = read_bills()
-    display_menu()
-    process_choice(bills)
-    write_bills(bills)  # before leaving the application salve the list of bills
+    def insert_bill(self, pi_company, pi_customer, pi_date, pi_amount, pi_deb_cred):
+        row = len(self.bills) + 1
+        self.bills[row][0] = pi_company
+        self.bills[row][1] = pi_customer
+        self.bills[row][2] = datetime.datetime.strptime(pi_date, '%Y-%m-%d').date().year
+        self.bills[row][3] = datetime.datetime.strptime(pi_date, '%Y-%m-%d').date().month
+        self.bills[row][4] = datetime.datetime.strptime(pi_date, '%Y-%m-%d').date().day
+        self.bills[row][5] = pi_amount
+        self.bills[row][6] = pi_deb_cred
+
+        
+    def process_choice(self):
+        choice = input('Please enter an option:') 
+        while choice != '5':    # check the version of python to identify the type of return from input function 
+            if choice == '1':
+                self.view_bills()
+                
+            elif choice == '2':
+                self.insert_bill()
+        
+            
+            choice = input('Please enter an option:') 
+
+
+            
+    def main(self):
+        self.read_bills()
+        self.display_menu()
+        self.process_choice()
+        self.write_bills()  # before leaving the application save the list of bills
     
 if __name__  == '__main__':
-    main()
+    bill_management = bill_management()
+    bill_management.main()
--- billManagement/bill_management_test.py
+++ billManagement/bill_management_test.py
@@ -1,25 +1,56 @@
 import unittest
 
-from bill_management import read_bills, write_bills
+from bill_management import bill_management
 
 class TestBillManagement(unittest.TestCase):
     
+    # this method is executed before each test method
+    def setUp(self):
+        self.bill_management = bill_management()
+      
+    
     def test_read_bills(self):
-        bills = read_bills()
-        self.assertEquals( 20, len(bills)) 
-        self.assertEquals( 'Electric Ireland', bills[0][0]) 
-        self.assertEquals( 'credit', bills[19][6]) 
+        self.bill_management.read_bills()
+        # initial file has 20 rows
+        self.assertEquals( 20, len(self.bill_management.bills)) 
+        # verify that the information in the first column in the first line is 'Eletric Ireland'
+        self.assertEquals( 'Electric Ireland', self.bill_management.bills[0][0])  
+        # verify that the information in the 7th column in the 20th line (last line/last column) is 'credit' 
+        self.assertEquals( 'credit', self.bill_management.bills[19][6]) 
         
     def test_write_bills(self):
-        bills = read_bills()
-        write_bills(bills)
-        bills = read_bills()
-        self.assertEquals( 20, len(bills)) # after read, write and read again the file should be the same
-        self.assertEquals( 'Electric Ireland', bills[0][0]) 
-        self.assertEquals( 'credit', bills[19][6]) 
+        self.bill_management.read_bills()   # read the initial file
+        self.bill_management.write_bills()  # write list of bills back to the file
+        self.bill_management.read_bills()   # read file again
+        # after read, write and read again, the file should be the same
+        # file should have 20 lines
+        self.assertEquals( 20, len(self.bill_management.bills)) 
+        # verify that the information in the first column in the first line is still 'Eletric Ireland'
+        self.assertEquals( 'Electric Ireland', self.bill_management.bills[0][0]) 
+        # verify that the information in the 7th column in the 20th line (last line/last column) is still 'credit'
+        self.assertEquals( 'credit', self.bill_management.bills[19][6]) 
         
-#    def test_get_message(self):
-#        self.assertEquals()
+    def test_insert_bill(self):
+        self.bill_management.read_bills()   # read the initial file
+        self.bill_management.insert_bill()
+        self.bill_management.write_bills()  # write list of bills back to the file
+        self.bill_management.read_bills()   # read the file again 
+        # the file should has 21 rows
+        self.assertEquals( 21, len(self.bill_management.bills)) 
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][0]) 
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][1]) 
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][2])
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][3]) 
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][4])
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][5])
+        
+        self.assertEquals( 'credit', self.bill_management.bills[20][6]) 
         
         
 if __name__  == '__main__':
